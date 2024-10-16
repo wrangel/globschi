36819a6987f43a4431e744e01ba0cb99351d5f90
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2024-10-16 18:53:22 +0200

    Implemented Debounce Window Resize

--- src/components/PortfolioGrid.js
+++ src/components/PortfolioGrid.js
@@ -1,10 +1,17 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect, useMemo } from "react";
 import Masonry from "react-masonry-css";
 import PortfolioItem from "./PortfolioItem";
 import ImagePopup from "./ImagePopup";
+import { debounce } from "../utils/utils";
 
 const PortfolioGrid = React.memo(({ items }) => {
   const [selectedItem, setSelectedItem] = useState(null);
+  const [breakpointColumns, setBreakpointColumns] = useState({
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  });
 
   const handleItemClick = useCallback((item) => {
     setSelectedItem(item);
@@ -14,17 +21,30 @@ const PortfolioGrid = React.memo(({ items }) => {
     setSelectedItem(null);
   }, []);
 
-  const breakpointColumnsObj = {
-    default: 4,
-    1100: 3,
-    700: 2,
-    500: 1,
-  };
+  const updateBreakpoints = useCallback(() => {
+    const width = window.innerWidth;
+    setBreakpointColumns((prevState) => ({
+      ...prevState,
+      default: width >= 1100 ? 4 : width >= 700 ? 3 : width >= 500 ? 2 : 1,
+    }));
+  }, []);
+
+  const debouncedUpdateBreakpoints = useMemo(
+    () => debounce(updateBreakpoints, 250),
+    [updateBreakpoints]
+  );
+
+  useEffect(() => {
+    window.addEventListener("resize", debouncedUpdateBreakpoints);
+    return () => {
+      window.removeEventListener("resize", debouncedUpdateBreakpoints);
+    };
+  }, [debouncedUpdateBreakpoints]);
 
   return (
     <>
       <Masonry
-        breakpointCols={breakpointColumnsObj}
+        breakpointCols={breakpointColumns}
         className="masonry-grid"
         columnClassName="masonry-grid_column"
       >

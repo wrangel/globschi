ae7536ba5affaae675d48428cc370c9506a3c36f
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2024-10-23 20:42:16 +0200

    Finalized branch optimizations

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./styles/main.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Initialize the root element with React 18's createRoot
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the App component wrapped in StrictMode
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
+// Performance measurement setup
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// to log results (for example: console.log) or send to an analytics endpoint.
+// Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);

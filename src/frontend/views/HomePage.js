189697e9cc9d2f34e30538771041041ef431a772
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2024-11-11 17:02:38 +0100

    Let the hamburger menu disappear behind the popup

--- src/frontend/views/HomePage.js
+++ src/frontend/views/HomePage.js
@@ -1,12 +1,25 @@
 // src/views/HomePage.js
-
-import React from "react";
+import React, { useState } from "react";
 import PortfolioGrid from "../components/PortfolioGrid";
 import styles from "../styles/Home.module.css";
 import { useItems } from "../hooks/useItems";
+import HamburgerMenu from "../components/HamburgerMenu"; // Import HamburgerMenu
+import ViewerPopup from "../components/ViewerPopup"; // Import ViewerPopup
 
 function HomePage() {
   const { items, isLoading, error } = useItems();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+    setIsModalOpen(true);
+  };
+
+  const handleClosePopup = () => {
+    setIsModalOpen(false);
+    setSelectedItem(null);
+  };
 
   if (isLoading) {
     return <div className={styles.homePage}>Loading...</div>;
@@ -22,10 +35,20 @@ function HomePage() {
         <h1>Dronef Kollege von Globschi. Der arme Dronef versunken im See</h1>
       </div>
       {items.length > 0 ? (
-        <PortfolioGrid items={items} />
+        <PortfolioGrid items={items} onItemClick={handleItemClick} />
       ) : (
         <p>No items to display.</p>
       )}
+      {/* Render ViewerPopup if an item is selected */}
+      {isModalOpen && (
+        <ViewerPopup
+          item={selectedItem}
+          isOpen={isModalOpen}
+          onClose={handleClosePopup}
+        />
+      )}
+      {/* Always render Hamburger Menu */}
+      <HamburgerMenu />
     </div>
   );
 }

ddada4845ef00429722ec0f931fdb979d0b1240b
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2024-11-15 09:34:39 +0100

    Added css to loading error handler

--- src/frontend/components/LoadingErrorHandler.js
+++ src/frontend/components/LoadingErrorHandler.js
@@ -2,14 +2,23 @@
 
 import React from "react";
 import PropTypes from "prop-types";
+import styles from "../styles/LoadingErrorHandler.module.css";
 
 const LoadingErrorHandler = ({ isLoading, error, children }) => {
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className={styles.container}>
+        <p className={styles.message}>Loading...</p>
+      </div>
+    );
   }
 
   if (error) {
-    return <div>Error: {error}</div>;
+    return (
+      <div className={styles.container}>
+        <p className={`${styles.message} ${styles.error}`}>Error: {error}</p>
+      </div>
+    );
   }
 
   return children;

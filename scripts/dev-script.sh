dcd853cb2206cf17a70debd1c9b47d56b8b62dc9
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2024-11-20 20:48:19 +0100

    Added run scripts

---
+++ scripts/dev-script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update 
+pnpm update
+pnpm install
+pnpm audit --fix
+pnpm prune
+pnpm update react-scripts
+pnpm self-update
+pnpm depcheck
+
+# Start the backend server
+echo "Starting backend server..."
+node ./src/backend/server.mjs &
+
+# Start the frontend server
+echo "Starting frontend server on port 3000..."
+PORT=3000 react-scripts start &
+
+# Wait for both processes to finish
+wait
---
+++ scripts/prod-script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Clean
+docker compose down --rmi all && docker system prune -af
+
+# Set the DOTENV_KEY and build images using docker-compose
+export DOTENV_KEY=$(npx dotenv-vault keys production)
+docker compose --env-file .env.production build
+
+# Log in to Docker registry (if needed)
+docker login
+
+# Get a list of all images on the system
+IMAGES=$(docker images --format "{{.Repository}}:{{.Tag}}")
+
+# Loop through each image and push it
+for IMAGE in $IMAGES; do
+    echo "Pushing $IMAGE..."
+    docker push "$IMAGE"
+done
+
+echo "All images pushed successfully."

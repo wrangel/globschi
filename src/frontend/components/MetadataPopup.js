e18cd4d7d2daf42cf6843435edca872256353f51
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2024-11-20 15:00:16 +0100

    Changed api key

--- src/frontend/components/MetadataPopup.js
+++ src/frontend/components/MetadataPopup.js
@@ -1,9 +1,9 @@
-// src/frontend/components/MetadataPopup.js
 import React from "react";
-import styles from "../styles/MetadataPopup.module.css"; // Ensure you have this CSS file
+import styles from "../styles/MetadataPopup.module.css";
 
 const MetadataPopup = ({ metadata, latitude, longitude, onClose }) => {
-  const googleMapsUrl = `https://www.google.com/maps/embed/v1/view?key=YOUR_API_KEY&center=${latitude},${longitude}&zoom=12&maptype=satellite`;
+  console.log("DDDDD", latitude, longitude);
+  const googleMapsUrl = `https://www.google.com/maps/embed/v1/view?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&center=${latitude},${longitude}&zoom=12&maptype=satellite`;
 
   return (
     <div className={styles.metadataPopup}>
@@ -11,17 +11,15 @@ const MetadataPopup = ({ metadata, latitude, longitude, onClose }) => {
         ×
       </button>
       <div className={styles.content}>
-        {" "}
-        {/* Wrapper for centering text */}
         <pre>{metadata}</pre>
-        <a
-          href={googleMapsUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.mapLink}
-        >
-          View on Map
-        </a>
+        <iframe
+          width="100%"
+          height="450"
+          frameBorder="0"
+          style={{ border: 0 }}
+          src={googleMapsUrl}
+          allowFullScreen
+        ></iframe>
       </div>
     </div>
   );

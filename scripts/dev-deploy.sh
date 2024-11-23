723dcc0750c536ffb107db0746631d405b8501c0
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2024-11-23 08:21:46 +0100

    Updated gitignore

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -16,11 +16,6 @@ if [[ "$1" == "-u" ]]; then
     pnpm self-update
     pnpm depcheck
 
-    brew update
-    brew cleanup
-    brew doctor 
-    brew autoremove
-
 else
     echo "Skipping updates..."
 fi

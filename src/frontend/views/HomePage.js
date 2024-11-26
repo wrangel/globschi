dedf815079168d7e1af3c52567cbda724584f739
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2024-11-26 17:19:59 +0100

    Implemented random clicker

--- src/frontend/views/HomePage.js
+++ src/frontend/views/HomePage.js
@@ -1,9 +1,15 @@
-// src/frontend/views/HomePage.js
-
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "../styles/Home.module.css";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleImageClick = () => {
+    const randomPage = Math.random() < 0.5 ? "/grid" : "/map";
+    navigate(randomPage);
+  };
+
   return (
     <div className={styles.homePage}>
       <div className={styles.textWrapper}>
@@ -12,7 +18,13 @@ const HomePage = () => {
         </h1>
       </div>
       <div className={styles.imageWrapper}>
-        <img src="/globschi.jpg" alt="Globschi" className={styles.image} />
+        <img
+          src="/globschi.jpg"
+          alt="Globschi"
+          className={styles.image}
+          onClick={handleImageClick}
+          style={{ cursor: "pointer" }}
+        />
       </div>
     </div>
   );

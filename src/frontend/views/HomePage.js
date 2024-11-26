cf61571e9241888aaaa8d9871b17d783845e10ab
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2024-11-26 17:14:07 +0100

    Almost

--- src/frontend/views/HomePage.js
+++ src/frontend/views/HomePage.js
@@ -1,13 +1,15 @@
 // src/frontend/views/HomePage.js
 
 import React from "react";
-import styles from "../styles/Home.module.css"; // Adjust the path if necessary
+import styles from "../styles/Home.module.css";
 
 const HomePage = () => {
   return (
     <div className={styles.homePage}>
       <div className={styles.textWrapper}>
-        <h1>Marvels From Above</h1>
+        <h1>
+          Marvels From <span className={styles.break}>Above</span>
+        </h1>
       </div>
       <div className={styles.imageWrapper}>
         <img src="/globschi.jpg" alt="Globschi" className={styles.image} />

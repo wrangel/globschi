6252855777eeb2f8f077dbe86506458da77c521d
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2024-11-27 20:46:13 +0100

    Fixed height

--- src/frontend/components/MetadataPopup.js
+++ src/frontend/components/MetadataPopup.js
@@ -1,13 +1,23 @@
-// src/frontend/components/MetadataPopup.js
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import styles from "../styles/MetadataPopup.module.css";
 
 const MetadataPopup = ({ metadata, latitude, longitude, onClose }) => {
   const [zoomLevel, setZoomLevel] = useState(18); // Initial zoom level
+  const [isBelowThreshold, setIsBelowThreshold] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsBelowThreshold(window.innerHeight < 515); // Adjust threshold as needed
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const googleMapsUrl = `https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&q=${latitude},${longitude}&zoom=${zoomLevel}&maptype=satellite`;
+  const googleMapsLink = `https://www.google.com/maps?q=${latitude},${longitude}&zoom=${zoomLevel}&maptype=satellite`;
 
   const zoomOutTwoSteps = () => {
     setZoomLevel((prevZoom) => Math.max(prevZoom - 2, 0)); // Decrease zoom level by 2
@@ -20,14 +30,25 @@ const MetadataPopup = ({ metadata, latitude, longitude, onClose }) => {
       </button>
       <div className={styles.content}>
         <pre>{metadata}</pre>
-        <iframe
-          className={styles.mapIframe}
-          width="100%"
-          style={{ height: "50vh" }} // Responsive height
-          src={googleMapsUrl}
-          title={`Map showing location at latitude ${latitude} and longitude ${longitude}`}
-          allowFullScreen
-        ></iframe>
+        {isBelowThreshold ? (
+          <a
+            href={googleMapsLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.mapLink}
+          >
+            View on map
+          </a>
+        ) : (
+          <iframe
+            className={styles.mapIframe}
+            width="100%"
+            style={{ height: "50vh" }} // Responsive height
+            src={googleMapsUrl}
+            title={`Map showing location at latitude ${latitude} and longitude ${longitude}`}
+            allowFullScreen
+          ></iframe>
+        )}
       </div>
     </div>
   );

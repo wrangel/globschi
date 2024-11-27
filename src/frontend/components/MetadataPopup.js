330c2a4431bac34a08e626db143687f2d8f21009
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2024-11-27 20:15:20 +0100

    Made popup responsive

--- src/frontend/components/MetadataPopup.js
+++ src/frontend/components/MetadataPopup.js
@@ -1,13 +1,17 @@
 // src/frontend/components/MetadataPopup.js
 
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 import styles from "../styles/MetadataPopup.module.css";
 
-// Establishing the google maps zoom level
-const zoomLevel = 15;
-
 const MetadataPopup = ({ metadata, latitude, longitude, onClose }) => {
+  const [zoomLevel, setZoomLevel] = useState(18); // Initial zoom level
+
   const googleMapsUrl = `https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&q=${latitude},${longitude}&zoom=${zoomLevel}&maptype=satellite`;
+
+  const zoomOutTwoSteps = () => {
+    setZoomLevel((prevZoom) => Math.max(prevZoom - 2, 0)); // Decrease zoom level by 2
+  };
 
   return (
     <div className={styles.metadataPopup}>
@@ -17,16 +21,26 @@ const MetadataPopup = ({ metadata, latitude, longitude, onClose }) => {
       <div className={styles.content}>
         <pre>{metadata}</pre>
         <iframe
+          className={styles.mapIframe}
           width="100%"
-          height="450"
-          style={{ border: 0 }}
+          style={{ height: "50vh" }} // Responsive height
           src={googleMapsUrl}
-          title={`Map showing location at latitude ${latitude} and longitude ${longitude}`} // Added title attribute
+          title={`Map showing location at latitude ${latitude} and longitude ${longitude}`}
           allowFullScreen
         ></iframe>
+        <button onClick={zoomOutTwoSteps} className={styles.zoomButton}>
+          Zoom Out Two Steps
+        </button>
       </div>
     </div>
   );
+};
+
+MetadataPopup.propTypes = {
+  metadata: PropTypes.string.isRequired,
+  latitude: PropTypes.number.isRequired,
+  longitude: PropTypes.number.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 
 export default MetadataPopup;

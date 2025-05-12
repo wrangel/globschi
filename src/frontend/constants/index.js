cd612f75a0e173d652ab6dac6eef803df971c142
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2025-05-12 20:08:27 +0200

    Fix ugly pins

--- src/frontend/constants/index.js
+++ src/frontend/constants/index.js
@@ -1,7 +1,5 @@
 // src/frontend/constants/index.js
 
-import L from "leaflet"; // Import Leaflet
-
 // Grid breakpoints
 export const GRID_BREAKPOINTS = {
   default: 4,
@@ -26,31 +24,5 @@ export const PANORAMA_MIN_FOV = 10;
 // Animation durations
 export const INTRO_ANIMATION_DURATION = 6000; // in milliseconds
 
-// Icon URLs
-export const ICON_URLS = {
-  RED_MARKER:
-    "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
-  MARKER_SHADOW:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-};
-
-// Icon sizes
-export const ICON_SIZES = {
-  MARKER: [25, 41],
-  MARKER_ANCHOR: [12, 41],
-  POPUP_ANCHOR: [1, -34],
-  SHADOW: [41, 41],
-};
-
-// Define redPinIcon
-export const redPinIcon = new L.Icon({
-  iconUrl: ICON_URLS.RED_MARKER,
-  shadowUrl: ICON_URLS.MARKER_SHADOW,
-  iconSize: ICON_SIZES.MARKER,
-  iconAnchor: ICON_SIZES.MARKER_ANCHOR,
-  popupAnchor: ICON_SIZES.POPUP_ANCHOR,
-  shadowSize: ICON_SIZES.SHADOW,
-});
-
 // Domain
-export const DOMAIN = "https://abstractaltitudes.com/"; // Ensure trailing slash: This avoids issues where some tools or search engines might treat URLs with and without a trailing slash as different.
+export const DOMAIN = "https://abstractaltitudes.com/"; // Ensure trailing slash

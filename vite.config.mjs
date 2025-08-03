32a07fba4ead8de1e5e8fbb4f203e9f05e1a6ff7
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2025-08-03 15:42:34 +0300

    add deps

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// If you need to expose environment variables,
-// make sure to rename them as VITE_* in .env files.
-
 export default defineConfig({
   plugins: [react()],
   server: {

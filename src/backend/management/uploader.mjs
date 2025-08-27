0926d75486f7a456f11f23619622d72b51035a46
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2025-08-27 18:47:00 +0200

    Add metadata

--- src/backend/management/uploader.mjs
+++ src/backend/management/uploader.mjs
@@ -1,21 +1,48 @@
-import { readdir } from "fs/promises";
+import { readdir, readFile } from "fs/promises";
 import path from "path";
+import ExifParser from "exif-parser";
 
 async function listFolders(directory) {
   try {
     const entries = await readdir(directory, { withFileTypes: true });
     for (const entry of entries) {
       if (entry.isDirectory()) {
-        console.log(entry.name); // folder name
-        // If you want to recurse into subfolders, call listFolders here
-        // await listFolders(path.join(directory, entry.name));
+        console.log(entry.name);
       }
     }
   } catch (err) {
     console.error("Error reading directory:", err);
   }
 }
-// TODO add logger
+
+async function readExifFromFirstJPEGInBearbeitet(parentDir) {
+  try {
+    const bearbeitetPath = path.join(parentDir, "bearbeitet");
+    const files = await readdir(bearbeitetPath);
+
+    const jpgFile = files.find(
+      (file) =>
+        file.toLowerCase().endsWith(".jpg") ||
+        file.toLowerCase().endsWith(".jpeg")
+    );
+    if (!jpgFile) {
+      console.log(`No JPEG file found in ${bearbeitetPath}`);
+      return;
+    }
+
+    // Read image file as Buffer
+    const filePath = path.join(bearbeitetPath, jpgFile);
+    const imgBuffer = await readFile(filePath);
+
+    // Parse EXIF data
+    const parser = ExifParser.create(imgBuffer);
+    const exifData = parser.parse();
+
+    console.log(`EXIF data for ${filePath}:`, exifData.tags);
+  } catch (err) {
+    console.error("Error reading EXIF ", err);
+  }
+}
 
 const inputDir = process.env.INPUT_DIRECTORY;
 
@@ -24,4 +51,14 @@ if (!inputDir) {
   process.exit(1);
 }
 
-listFolders(inputDir);
+(async () => {
+  // List folders:
+  const entries = await readdir(inputDir, { withFileTypes: true });
+  for (const entry of entries) {
+    if (entry.isDirectory()) {
+      const folderPath = path.join(inputDir, entry.name);
+      console.log("Folder:", entry.name);
+      await readExifFromFirstJPEGInBearbeitet(folderPath);
+    }
+  }
+})();
